16d13a031897f97b77f8bda0c02e6687092870be
Jyosna Bogari <jyosnabogari05@gmail.com>
2026-05-21 13:20:01 +0530

    Fix Google strategy model import case

--- backend/config/googleStrategy.js
+++ backend/config/googleStrategy.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import { UserTypeModel } from "../models/userTypeModel.js";
+import { UserTypeModel } from "../models/UserTypeModel.js";
 
 passport.use(
   new GoogleStrategy(
